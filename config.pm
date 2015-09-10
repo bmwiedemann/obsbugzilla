63a82f457eff15b5d0002204429a9cfe125f7ed8
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-09-10 13:37:29 +0200

    split config variables into separate module

---
+++ config.pm
@@ -0,0 +1,13 @@
+package config;
+
+our ($username,$password);
+our $apiserver="api.opensuse.org";
+our $namespace="openSUSE:";
+our $privatecomment=0;
+eval(`/bin/cat $ENV{HOME}/.bugzillarc`);
+if(!$username || !$password) {
+    die "need username and password specified in .bugzillarc";
+}
+our $buildserver=$config::apiserver; $buildserver=~s/api\./build./; # by convention
+
+1;
