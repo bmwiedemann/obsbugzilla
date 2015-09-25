af405a560b895b62eecc96f8d2ad0dfc76e9dee5
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-09-25 17:11:48 +0200

    config: require user+pass
    this should make it easier for others to get started

--- config.pm
+++ config.pm
@@ -5,6 +5,9 @@ our $apiserver="api.opensuse.org";
 our $namespace="openSUSE:";
 our $privatecomment=0;
 eval(`/bin/cat $ENV{HOME}/.bugzillarc`);
+if(!$username || !$password) {
+    die "need username and password specified in .bugzillarc";
+}
 our $buildserver=$config::apiserver; $buildserver=~s/api\./build./; # by convention
 
 1;
