2c9dfb330abb46667ee4d519ecf996e37ec5ae77
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-09-25 16:48:40 +0200

    split OBS source

--- checknewobs.pl
+++ checknewobs.pl
@@ -10,6 +10,7 @@ use MLDBM qw(DB_File Storable);
 use Fcntl;
 use config;
 use obssupport;
+use source::OBS;
 my %data;
 my $dbname="issuemention.dbm";
 tie(%data, "MLDBM", $dbname, O_RDWR|O_CREAT, 0666) or die "error opening DB: $!";
@@ -19,73 +20,14 @@ my %bugmap2=%bugmap1;
 sub diag(@) #{print @_,"\n"}
 {}
 
-sub get_requests($)
-{
-	my $ns=shift;
-	my @a=gmtime(time-24*60*60); $a[4]++; $a[5]+=1900;
-	my $since=sprintf("%04i-%02i-%02i",$a[5],$a[4],$a[3]);
-	open(my $f, "-|", qq!osc -A https://$config::apiserver api "/search/request?match=starts-with(action/target/\@project,'$ns')+and+(state/\@name='new'+or+state/\@name='review'+or+state/\@name='accepted')+and+state/\@when>='$since'"!) or die $!;
-	#open(my $f, "-|", qq!osc api "/search/request?match=starts-with(action/target/\@project,'$ns')+and+(state/\@name='new'+or+state/\@name='review')+and+state/\@when>='$since'"!) or die $!;
-	#open(my $f, "<", "request.new.xml") or die $!;
-	local $/;
-	my $xml=<$f>;
-	close $f;
-	return $xml;
-}
-
-my $requests=get_requests($config::namespace);
-#die length($requests);
-if(!$requests || $requests!~m{<collection matches=}) {
-	system('echo "OBS SR source failed" | mailx -s OBS bwiedemann@suse.de');
-	print "OBS api failed\n" ; exit 17
-} # opensuse site failed
-my $reqdata=XMLin($requests, ForceArray=>['request','action','history'], keyattr=>['id']);
-$requests=$reqdata->{request};
-use JSON::XS; my $coder = JSON::XS->new->ascii->pretty->allow_nonref->allow_blessed->convert_blessed;
-#print $coder->encode($reqdata);
-foreach my $sr (sort keys %$requests) {
-	my $data=$requests->{$sr};
-	my ($type,$targetdistri, $package);
-   # reduce spamminess by skipping requests that are no more interesting
-	next if !$data->{state} || $data->{state}{name} =~ m/deleted|revoked|superseded/;
-	foreach my $a (@{$data->{action}}) {
-		next unless $a->{target};
-		next unless $a->{type} =~ m/submit|maintenance_incident/;
-		my $p=$a->{target}->{releaseproject} || $a->{target}->{project};
-		next unless $p && $p=~m/^$config::namespace(.*)/;
-		my $targetdistri1=$1;
-		$targetdistri1=~s/:Update$//;
-		$p=$a->{target}->{package} || $a->{source}->{package};
-		next unless $p;
-		next if $p eq "patchinfo";
-		$p=~s/_NonFree_Update//;
-		$p=~s/\.openSUSE_\d\d\.\d(_Update)?//;
-		$p=~s/\.SUSE_SLE-\d\d(-SP\d)?_Update//;
-		$targetdistri->{$targetdistri1}=1;
-		$package->{$p}=1;
-		$type=$a->{type};
-		#print "$sr: $type @$targetdistri / $package\n";
-	}
-	next unless $type;
-	my $descr=$data->{description}||"";
-	my $lt=qr/(?:<|&lt;)/;
-	my $gt=qr/(?:>|&gt;)/;
-	if($type eq "delete") {
-		if($descr=~m/openSUSE:Factory/) {
-			#print "SR:$sr $type $descr";
-		}
-		next;
-	}
-	$targetdistri=join("+", keys %$targetdistri);
-	$package=join("+", keys %$package);
-	foreach my $mention ($descr=~m/\b(\w+#\d{3,})/g) {
-		$mention=~s/boo#(\d{6,7}\b)/bnc#$1/; #bugzilla.opensuse.org
-		$mention=~s/bsc#(\d{6,7}\b)/bnc#$1/; #bugzilla.suse.com
-		$mention=~s/bug#(\d{6,7}\b)/bnc#$1/; # TODO: needs update when bug numbers go higher
-#		print "$sr ($targetdistri / $package) mention: $mention\n";
-		addentry(\%bugmap2, $mention, $sr);
-		addsrinfo($sr, $targetdistri, $package);
-	}
+my $mentions = source::OBS::fetch();
+foreach my $mention (keys %$mentions) {
+    foreach my $e (@{$mentions->{$mention}}) {
+        my ($sr, $targetdistri, $package) = ($e->{sr}, $e->{distri}, $e->{package});
+        #print "$sr ($targetdistri / $package) mention: $mention\n";
+        addentry(\%bugmap2, $mention, $sr);
+        addsrinfo($sr, $targetdistri, $package);
+    }
 }
 
 # check which entries were new
