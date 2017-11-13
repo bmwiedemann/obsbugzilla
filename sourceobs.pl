1723eecccf4212d9a5b921b5d5e657a151715820
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2017-11-13 13:55:10 +0100

    add sourceobs.pl

---
+++ sourceobs.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+use strict;
+# usage: ./sourcerabbit.pl
+# fetches rabbitmq events and stores entries in queue dir
+use JSON::XS;
+use lib ".";
+use config;
+use common;
+use source::OBS;
+
+sub diag(@) #{print @_,"\n"}
+{}
+
+my $r=source::OBS::fetch();
+foreach my $m (keys %$r) {
+    my $data=$r->{$m};
+    foreach my $m (keys(%$data)) {
+        diag("adding $m ".encode_json($data->{$m}));
+        common::enqueue($data->{$m});
+    }
+}
