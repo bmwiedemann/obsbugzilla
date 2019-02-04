9d6381c46aa2b4699adacd38846ad364a4914594
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-02-04 09:17:55 +0100

    updatejiraprojects: Keep old json until sanity check passed
    Without this, there were some instances of 07:02 cron job
    erroring out with
    data/jiraproject.json:  at common.pm line 76.
    Compilation failed in require at ./sourceobs.pl line 9.
    BEGIN failed--compilation aborted at ./sourceobs.pl line 9.

--- updatejiraprojects.sh
+++ updatejiraprojects.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # assumes ~/.netrc has credentials
+j=data/jiraproject.json
 mkdir -p data
-exec curl -s -n https://jira.suse.de/rest/api/2/project > data/jiraproject.json
+curl -s -n https://jira.suse.de/rest/api/2/project > $j.new
+grep -q '"SCRD"' $j.new && mv $j.new $j
