a43613f8c22ca03177ada038911ac980ecdf43f2
Bernhard M. Wiedemann <bwiedemann@suse.de>
2020-12-17 03:24:06 +0100

    Update updatejiraprojects.sh
    jira.suse.de only returned redirects
    and the SCRD project was dropped

--- updatejiraprojects.sh
+++ updatejiraprojects.sh
@@ -2,5 +2,5 @@
 # assumes ~/.netrc has credentials
 j=data/jiraproject.json
 mkdir -p data
-curl -s -n https://jira.suse.de/rest/api/2/project > $j.new
-grep -q '"SCRD"' $j.new && mv $j.new $j
+curl -s -n https://jira.suse.com/rest/api/2/project > $j.new
+grep -q '"ENGINFRA"' $j.new && mv $j.new $j
