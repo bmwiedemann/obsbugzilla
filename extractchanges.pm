ecfb3a5a058e920ac16bb6f9d49d4619b31f9487
Bernhard M. Wiedemann <bwiedemann@suse.de>
2021-07-08 07:49:22 +0200

    Make a non-greedy match
    as it should perform better

--- extractchanges.pm
+++ extractchanges.pm
@@ -6,7 +6,7 @@ sub extract_changes($$)
     my $lines = <$fd>;
     my $re = qr/\(?(boo|bsc|bnc)\s?#$bugid\)?/;
     #print $lines;
-    if($lines =~ m/\n\s*[+*-] ((?:[^+*-]|(?<!\s).(?! )){1,300}?$re[^\n]*)/s) {
+    if($lines =~ m/\n\s*[+*-] ((?:[^+*-]|(?<!\s).(?! )){1,300}$re[^\n]*)/s) {
         $_ = $1;
 	s/$re[. ]*$//;
 	s/^\s*[+*-] //;
