96dd0fdff515457c3ec9ce851941ec24d8d08a67
Bernhard M. Wiedemann <bwiedemann@suse.de>
2021-07-08 07:39:51 +0200

    Allow to capture text after bugref

--- extractchanges.pm
+++ extractchanges.pm
@@ -6,8 +6,9 @@ sub extract_changes($$)
     my $lines = <$fd>;
     my $re = qr/\(?(boo|bsc|bnc)\s?#$bugid\)?/;
     #print $lines;
-    if($lines =~ m/\n\s*[+*-] ((?:[^+*-]|(?<!\s).(?! )){1,300}?)$re/s) {
+    if($lines =~ m/\n\s*[+*-] ((?:[^+*-]|(?<!\s).(?! )){1,300}?$re[^\n]*)/s) {
         $_ = $1;
+	s/$re[. ]*$//;
 	s/^\s*[+*-] //;
 	s/[\n ]+/ /g;
 	s/[\n (]+$//;
