418d6791d913089054fd831bbbd95d3254d82fc7
Bernhard M. Wiedemann <bwiedemann@suse.de>
2025-11-24 10:39:02 +0100

    Process gitea events

--- sourcerabbit.pl
+++ sourcerabbit.pl
@@ -8,6 +8,7 @@ use config;
 use common;
 use source::rabbitmq;
 use source::OBS;
+use source::gitea;
 
 sub diag(@) {print @_,"\n"}
 #{}
@@ -17,7 +18,12 @@ $SIG{ALRM} = sub {source::rabbitmq::close(); exit 0};
 source::rabbitmq::init();
 while(my $data=source::rabbitmq::fetchone()) {
     alarm(24*3600);
-    my $srmentions=source::OBS::getsrmentions($data);
+    my $srmentions;
+    if($data->{routing_key} =~ /\.src\..*pull_request/) {
+        $srmentions=source::gitea::getprmentions($data);
+    } else {
+        $srmentions=source::OBS::getsrmentions($data);
+    }
     foreach my $m (@$srmentions) {
         diag("adding $data->{number} ".encode_json($m));
 	common::enqueue($m);
