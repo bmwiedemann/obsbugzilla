9441d929fafedb4a636011126ba09fc8e040d293
Bernhard M. Wiedemann <githubbmw@lsmod.de>
2026-01-07 11:54:02 +0000

    Support JSON from obsbugzilla_source.py --raw
    called like
    python3 obsbugzilla_source.py --user "$GITEA_BOT_USER" --scm=git --platform=gitea --gitea-url=https://src.suse.de/ fetch --since=2025-12-10T00:00:00Z --filter 'b[ns]c#|boo#|jsc#' --raw

--- source/gitea.pm
+++ source/gitea.pm
@@ -7,12 +7,12 @@ our $jiraprojectre = join("|", common::getjiraprojects());
 sub getprmentions($)
 {
     my $data=shift;
-    my $descr=$data->{pull_request}{title}.$data->{pull_request}{body};
-    my $sr = $data->{pull_request}{url};
+    my $descr=$data->{title}.$data->{body};
+    my $sr = $data->{url};
     $sr =~ s!https://[^/]+/!!; $sr=~s!/!:!g; $sr=~s/:pulls:/:/;
     my $package;
     $sr =~ m/([^:]+):\d+$/ and $package=$1;
-    my $targetdistri = $data->{pull_request}{base}{ref};
+    my $targetdistri = $data->{base}{ref};
     my @jiramentionids=($descr=~m/\b(js[cd]#(?:$jiraprojectre)-\d+)/go);
     my @mentionids=@jiramentionids;
     push(@mentionids, ($descr=~m/\b(\w+#\d{3,})/g));
@@ -24,7 +24,7 @@ sub getprmentions($)
         $mention=~s/boo#(\d{6,7}\b)/bnc#$1/; #bugzilla.opensuse.org
         $mention=~s/bsc#(\d{6,7}\b)/bnc#$1/; #bugzilla.suse.com
         $mention=~s/bug#(\d{6,7}\b)/bnc#$1/; # TODO: needs update when bug numbers go higher
-        push(@mentions, {id=>$sr, url=>$data->{pull_request}{url}, distri=>$targetdistri, extra=>"$targetdistri / $package", mention=>$mention});#, time=>$when});
+        push(@mentions, {id=>$sr, url=>$data->{url}, distri=>$targetdistri, extra=>"$targetdistri / $package", mention=>$mention});#, time=>$when});
     }
     return \@mentions;
 }
